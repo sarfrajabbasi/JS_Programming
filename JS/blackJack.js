b6316f828d5c1ac663312f9b4722169e36996b4e
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-22 09:25:25 +0530

    js programming

--- JS/blackJack.js
+++ JS/blackJack.js
@@ -9,23 +9,56 @@ let firstCard = 12;
 let secondCard = 9;
 
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+// create var isalive and assign the value true and flip the value in aproprate code
+let isAlive = true;
+
+// add message var and assign ""; and reassign var to logging out
+let message = "";
+
 
 console.log(sum);
 
-if(sum < 21){
-    console.log('Do you want to draw a new card');
-}else if(sum === 21){
-    console.log("wohoo! you've got blackjack!");
-}else if(sum > 21){
-    console.log("You 're of thr game");
+if (sum < 21) {
+  message = "Do you want to draw a new card";
+} else if (sum === 21) {
+  message = "wohoo! you've got blackjack!";
+  hasBlackJack = true
+} else if (sum > 21) {
+  message = "You 're of thr game";
+  isAlive = false
 }
+
+// Cash OUT!
+
+console.log(hasBlackJack);
+console.log(isAlive);
+console.log(message);
+
 
 // check if the persion is old enough to enter the nightclub(21)
 
 let age = 21;
 
-if(age<21){
-    console.log('You cant enter the club!');
-}else{
-    console.log("Welcome");
+if (age <= 20) {
+  console.log("You can't enter the club!");
+} else {
+  console.log("Welcome");
 }
+
+// If/Else...statement
+
+age = 100;
+
+// check if the person is elegible for a birthday card from the king!
+
+if (age <= 99) {
+  console.log("Not elegible");
+} else if (age === 100) {
+  console.log("Here is your birthday card from the king!");
+} else {
+  console.log("Not elegible,you have already gotten one");
+}
+
+
+
