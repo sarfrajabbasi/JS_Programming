6b68605a9abd2aa6ea2396da819ce146a32b1286
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-24 10:43:28 +0530

    js programming

--- JS/chromeExtent.js
+++ JS/chromeExtent.js
@@ -1,8 +1,26 @@
+
+// create the myleads array and InputEl.
+
+// Create two variables
+
+// myLeads --> should be assigned to an empty array
+// inputEl --> should be assigned to the text input field.
+
+let myLeads = [];
+const inputBtn = document.getElementById('input-btn');
+const inputEl = document.getElementById('input-el');
+
 // log out 'button clicked' when the user clicks the "SAVE INPUT" button
 
-const inputBtn = document.getElementById('input-btn');
-console.log(inputBtn);
+
 inputBtn.addEventListener('click',()=>{
-    console.log("Button clicked from addEventListener");
+// push the value www.asda.com to myArr when the inputbtn clicked.
+// push the value from input filed into myLeads arr.
+myLeads.push(inputEl.value)
+console.log(myLeads);
+    
 });
+
+// render items/logout the items in the myLeads arr using a for loop
+
 
---
+++ JS/practice1.js
@@ -0,0 +1,6 @@
+// const box = document.querySelector('.box1');
+
+// box.addEventListener('click',()=>{
+// console.log("I Want to open the box! when it's clicked");
+// })
+
