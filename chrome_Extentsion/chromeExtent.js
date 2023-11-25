7415ed8d9a7cdbcc0aacdc071b4d7f6f29f6d9a1
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-25 10:49:26 +0530

    js programming

--- chrome_Extentsion/chromeExtent.js
+++ chrome_Extentsion/chromeExtent.js
@@ -6,21 +6,33 @@
 // inputEl --> should be assigned to the text input field.
 
 let myLeads = [];
+
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulList = document.querySelector("#ul-list");
+
+// get leads from the localStorage
+// store it in a variable leadsFromLocalStorage
+// log out variable
+
+let leadsFromLocalStorage =JSON.parse(localStorage.getItem('myleads'));
+console.log(leadsFromLocalStorage);
 
 // log out 'button clicked' when the user clicks the "SAVE INPUT" button
 
 inputBtn.addEventListener("click", () => {
   // push the value www.asda.com to myArr when the inputbtn clicked.
   // push the value from input filed into myLeads arr.
-
+  
   myLeads.push(inputEl.value);
 
   // clear the input field
   inputEl.value = "";
-  renderLeads();
+
+  // save the myLeads array to localStorage
+  localStorage.setItem('myleads',JSON.stringify(myLeads))
+  renderLeads(); 
+
 });
 
 // wrap the code in functions:-------
@@ -53,4 +65,23 @@ function renderLeads() {
   console.log(inputEl.value);
 }
 
-// pracitce
+
+// Practice:--
+
+// storing array in localStorage
+
+// localStorage.setItem("myLeads",JSON.stringify([1,2,3,4,5,"sa",{a:"a"}]));
+
+// console.log(JSON.parse(localStorage.getItem("myLeads")));
+// // localStorage.clear()
+
+
+let currViewer = null;
+currViewer = ["Jane","nick"];
+currViewer = null;
+if(currViewer){
+  console.log("yes the watch you");
+}else{
+  console.log("no viewer watch");
+
+}
