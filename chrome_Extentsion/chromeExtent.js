51d43a4f244a379551b70862b188396e4f49e2d4
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-25 12:05:39 +0530

    js programming

--- chrome_Extentsion/chromeExtent.js
+++ chrome_Extentsion/chromeExtent.js
@@ -8,8 +8,8 @@
 let myLeads = [];
 
 const inputBtn = document.getElementById("input-btn");
-const deleteBtn = document.getElementById('delete-btn');
-const tabBtn = document.getElementById('tab-btn');
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 const inputEl = document.getElementById("input-el");
 const ulList = document.querySelector("#ul-list");
@@ -18,49 +18,54 @@ const ulList = document.querySelector("#ul-list");
 // store it in a variable leadsFromLocalStorage
 // log out variable
 
-const leadsFromLocalStorage =JSON.parse(localStorage.getItem('myleads'));
-
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myleads"));
 
 // check if leadsfrom localStore is truthy and if so,set myleads to its value and call renderLeads()
-if(leadsFromLocalStorage){
+if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  render(myLeads)
+  render(myLeads);
 }
 
-const tabs = [
-  {url:"https://github.com/sarfrajabbasi"}
-]
 
 // log out 'button clicked' when the user clicks the "SAVE INPUT" button
 
 inputBtn.addEventListener("click", () => {
   // push the value www.asda.com to myArr when the inputbtn clicked.
   // push the value from input filed into myLeads arr.
-  
+
   myLeads.push(inputEl.value);
 
   // clear the input field
   inputEl.value = "";
 
   // save the myLeads array to localStorage
-  localStorage.setItem('myleads',JSON.stringify(myLeads))
-  render(myLeads); 
-
+  localStorage.setItem("myleads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
 // Listen for double clicks on the delete-btn  and clear localstorage,myleads and DOM
 
-deleteBtn.addEventListener('dblclick',()=>{
+deleteBtn.addEventListener("dblclick", () => {
   localStorage.clear();
   // ulList.innerHTML = ""
-  myLeads =[];
-  render(myLeads)
-})
+  myLeads = [];
+  render(myLeads);
+});
 
 // listen for click on tabBtn and log url
-tabBtn.addEventListener('click',()=>{
-  console.log("url");
-})
+tabBtn.addEventListener("click", () => {
+  // grab the url of the current tab!
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true},
+    function(tabs) {
+      myLeads.push(tabs[0].url);
+      localStorage.setItem("myleads", JSON.stringify(myLeads));
+      render(myLeads);
+    });
+  
+  
+});
 // wrap the code in functions:-------
 
 function render(leads) {
@@ -87,9 +92,7 @@ function render(leads) {
   // DOM manupulation come with cost
 
   ulList.innerHTML = listItems;
-
 }
-
 
 // Practice:--
 
@@ -99,7 +102,6 @@ function render(leads) {
 
 // console.log(JSON.parse(localStorage.getItem("myLeads")));
 // // localStorage.clear()
-
 
 // js singalize emtpyness (undefined)
 // devlop singalize emtpyness (null)
@@ -121,16 +123,16 @@ function render(leads) {
 // let trueOfFalse = Boolean(-0);//f
 // console.log(trueOfFalse);
 
-const welcomeEl = document.getElementById('welcome-el');
+const welcomeEl = document.getElementById("welcome-el");
 
-function greetUser(name="User",greet="Hello,"){
-  welcomeEl.textContent = `${greet}, ${name} :)`
+function greetUser(name = "User", greet = "Hello,") {
+  welcomeEl.textContent = `${greet}, ${name} :)`;
 }
-greetUser("sarfraj ","kya haal-Chaal ha bhai")
+greetUser("sarfraj ", "kya haal-Chaal ha bhai");
 
-function firstItemInArr(arr){
-  return arr[0]
+function firstItemInArr(arr) {
+  return arr[0];
 }
-const first  = firstItemInArr(["sarfraj",2,3,4]);
+const first = firstItemInArr(["sarfraj", 2, 3, 4]);
 
 console.log(first);
